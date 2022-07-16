d55021ba470197a1f58e38e9793c5ab4f56721c2
ryan-1337 <ryanaouiche@gmail.com>
2022-07-17 00:39:51 +0200

    notes v0.2

--- intervenant/pages/Home/notes.php
+++ intervenant/pages/Home/notes.php
@@ -36,6 +36,18 @@ $res->execute();
                 ?>
             </select>
 
+            <label for="classe" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400 text-center">Selectionné classe</label>
+            <select id="classe" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option selected>Choisir classe</option>
+                <?php
+                foreach ($res as $classe) {
+                 ?>   
+                <option value="<?php echo $classe['nom_classe'];?>"><?php echo $classe['nom_classe'];?></option>
+                <?php
+                }
+                ?>
+            </select>
+
             <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Validez</button>
             </div>
         </form>
